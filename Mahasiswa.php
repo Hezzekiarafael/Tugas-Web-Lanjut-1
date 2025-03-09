64bf7db72a017523ddf3ce4841059da703683509
Hezzekiarafael <hrafaelmiracle@gmail.com>
2025-03-09 17:03:00 +0700

    Menambahkan Tugas

---
+++ Mahasiswa.php
@@ -0,0 +1,15 @@
+<?php
+class Mahasiswa {
+    public $nim;
+    public $nama;
+
+    public function setData($nim, $nama) {
+        $this->nim = $nim;
+        $this->nama = $nama;
+    }
+    public function getData()
+    {
+        return[$this->nim, $this->nama];
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form NIM dan Nama</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    include 'Mahasiswa.php';
+
+    $mahasiswa_1 = new Mahasiswa();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nim = $_POST['nim'];
+        $nama = $_POST['nama'];
+
+        $mahasiswa_1->setData($nim, $nama);
+    }
+    ?>
+    <div class="container">
+        <div class="title">Form Nim & Nama</div>
+            <div class="content">
+                <form  class=" form-group"action="" method="post">
+
+                <label for="nim">NIM :</label>
+                <input type="text" id="nim" name="nim" required><br><br>
+          
+                <label for="nama">Nama :</label>
+                <input type="text" id="nama" name="nama" required><br><br>
+         
+
+                <div class="button">
+                <button type="submit">Submit</button>
+                </div>
+                </form>
+                <br>   
+                <div class="hasil" >
+                <p><?= $mahasiswa_1->getData()[1]; ?>&nbsp;&nbsp;<?= $mahasiswa_1->getData()[0]; ?></p>
+
+                </div>
+            </div>
+    </div>
+
+</body>
+</html>
